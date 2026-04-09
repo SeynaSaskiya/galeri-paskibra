635f0b6ba5285b121336bfc57da7e9c95b7274f0
SeynaSaskiya <seynasaskiya@gmail.com>
2026-04-09 13:35:26 +0700

    menambahkan components

---
+++ resources/views/components/_footer.blade.php
@@ -0,0 +1,12 @@
+    <!-- FOOTER -->
+    <footer style="margin-top: 80px; border-top: 1px solid #EEF0F7; background: white;">
+        <div class="max-w-[1130px] mx-auto flex justify-between items-center" style="padding: 24px 0;">
+            <div class="flex items-center" style="gap: 8px;">
+                <div>
+                    <img src="{{ asset('assets/images/icons/paskibra.logo.jpeg') }}" alt="icon" style="width: 34px; height: 34px;" />
+                </div>
+                <span class="font-semibold text-[#A3A6AE]" style="font-size: 12px;">Paskibra SMK Al-Hafidz Leuwiliang</span>
+            </div>
+            <p class="text-[#A3A6AE]" style="font-size: 12px;">&copy; {{ date('Y') }} · Paskibra SMK Al-Hafidz Leuwiliang</p>
+        </div>
+    </footer>
